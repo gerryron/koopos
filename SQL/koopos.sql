0e0a043babf689446782d4287583b4b680aae2f1
Gerryron <Gerryron@btpnsyariah.com>
2023-04-08 11:44:16 +0700

    <SQL> add transaction table

--- SQL/koopos.sql
+++ SQL/koopos.sql
@@ -21,6 +21,31 @@ create table if not exists category (
     primary key (id)
 );
 
+create table if not exists transaction (
+	id int not null auto_increment,
+    transaction_number varchar(255) unique not null,
+    amount int not null,
+    total_price decimal(21,2),
+    profit decimal(21,2),
+    created_date datetime default current_timestamp,
+    updated_date datetime default current_timestamp on update current_timestamp,
+    primary key (id)
+);
+
+create table if not exists transaction_details (
+	id int not null auto_increment,
+    transaction_number varchar(255) not null,
+    inventory_id int not null,
+    amount int not null,
+    price decimal(21,2),
+    created_date datetime default current_timestamp,
+    updated_date datetime default current_timestamp on update current_timestamp,
+    primary key (id),
+    constraint constr_transactionDetails_transaction_fk 
+		foreign key transaction_fk (transaction_number) REFERENCES transaction (transaction_number)
+        on delete cascade on update cascade
+);
+
 create table if not exists inventory_categories ( 
 	inventory_id int not null,
     category_id int not null,
