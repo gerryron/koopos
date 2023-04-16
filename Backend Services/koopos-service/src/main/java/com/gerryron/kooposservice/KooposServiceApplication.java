ed43614f373a21ded0a5e3b16354fb77cce6662a
gerryron <gerryron02@gmail.com>
2023-04-17 02:13:57 +0700

    <Koopos Service> initial project

---
+++ Backend Services/koopos-service/src/main/java/com/gerryron/kooposservice/KooposServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gerryron.kooposservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooposServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KooposServiceApplication.class, args);
+	}
+
+}
---
+++ Backend Services/koopos-service/src/test/java/com/gerryron/kooposservice/KooposServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerryron.kooposservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KooposServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
